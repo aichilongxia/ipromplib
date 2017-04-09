45210aad53223bde48797f39c765bf32298a2cff
chenlx92 <chenlx92@outlook.com>
2017-04-09 22:21:46 +0800

    show the via point using origin code

--- src/simple.py
+++ src/simple.py
@@ -5,31 +5,35 @@ import ipromp
 import numpy as np
 import matplotlib.pyplot as plt
 
-# create a ProMPs object
-p = ipromp.ProMP()
-
 # close the current windows
 plt.close()
+
+# create a ProMPs object
+p = ipromp.ProMP()
 
 # Generate and plot trajectory Data
 x = np.arange(0,1.01,0.01)           # time points for trajectories
 nrTraj=30                            # number of trajectoreis for training
 sigmaNoise=0.02                      # noise on training trajectories
-A = np.array([.2, .2, .01, -.05])
+A = np.array([.2, .2, .01, -.05])    # the weight of different func 
 X = np.vstack( (np.sin(5*x), x**2, x, np.ones((1,len(x))) ))
 
-Y = np.zeros( (nrTraj,len(x)) )
+# add demonstration
 for traj in range(0, nrTraj):
     sample = np.dot(A + sigmaNoise * np.random.randn(1,4), X)[0]
     label = 'training set' if traj==0 else ''
     plt.plot(x, sample, 'grey', label=label)
     p.add_demonstration(sample)
-    
-p.plot(x=p.x, legend='trained module                                                                   ', color='r')
-    
-p.set_goal(0.05)
-#plt.plot(1.0, 0.05, 'o', label="point",color='red')
 
+# add via point as observation
+p.set_start(-0.04)
+p.add_viapoint(0.1, 0.055)
+p.add_viapoint(0.2, 0.130)
+
+# plot the trained model and via point
+p.plot(x=p.x, color='r')
+
+# plot the generated traj
 plt.plot(p.x, p.generate_trajectory(), 'g', label='generated traj',linewidth=3)
 
 plt.legend()
