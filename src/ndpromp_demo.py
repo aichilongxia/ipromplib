f8beb524e3f757fb275dea59c8ebc5db61865bf1
chenlx92 <chenlx92@outlook.com>
2017-04-13 10:50:36 +0800

    the init version

---
+++ src/ndpromp_demo.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# Filename: ndpromp_demo.py
+
+import ipromp
+import numpy as np
+import matplotlib.pyplot as plt
+
+# close the current windows
+plt.close()
+
+# create a ProMP object
+p = ipromp.NDProMP(2)
+
+# Generate and plot trajectory Data
+
+
+# add demonstration
+
+
+# add via point as observation
+
+
+# plot the trained model and via point
+
+
+# plot the generated traj
